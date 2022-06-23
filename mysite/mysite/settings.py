81c754597bacda47ad72fd20fbfe91f73b98627b
Martina <marti.89.rn@gmail.com>
2022-06-23 23:31:30 +0200

    fix CSS mimetype

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -13,7 +13,7 @@ import os
 from pathlib import Path
 import mimetypes
 mimetypes.add_type("text/css", ".css", True)
-mimetypes.add_type("text/html", ".css", True)
+mimetypes.add_type("text/html", ".html", True)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
